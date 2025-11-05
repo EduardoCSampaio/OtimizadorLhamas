75fb1c45284a38012949b80c0f4816d7f306cb2f
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-11-05 17:40:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3995665732-78a10",
+  "appId": "1:278809067081:web:3ea25268caaa25df850998",
+  "apiKey": "AIzaSyCo0LNsncIanUjWl3uf9EmujAeHJZOlE-0",
+  "authDomain": "studio-3995665732-78a10.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "278809067081"
+};
